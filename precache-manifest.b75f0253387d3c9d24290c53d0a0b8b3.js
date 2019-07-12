eac020da0470a876c00c01c412ee46e6ed409f10
crystadavis1119 <crystadavis1119@hotmail.com>
2019-07-12 16:16:23 -0500

    Updates

---
+++ precache-manifest.b75f0253387d3c9d24290c53d0a0b8b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48bc2d33479b98620232af1f2c3708ae",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "4a5c0a038cc4b9c8315d",
+    "url": "/project-4-assessment/static/css/main.c0b561d0.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/project-4-assessment/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "4a5c0a038cc4b9c8315d",
+    "url": "/project-4-assessment/static/js/main.b7f6359e.chunk.js"
+  },
+  {
+    "revision": "aa123970de1f72af151a",
+    "url": "/project-4-assessment/static/js/runtime~main.4998de7c.js"
+  }
+]);
